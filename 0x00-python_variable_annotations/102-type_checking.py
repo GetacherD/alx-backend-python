3ee1ab05a27117f4b14a2c6a901220177882ba61
GetacherD <getacher.d@gmail.com>
2023-01-05 22:45:38 +0300

    first

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+annotation demos
+"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple,
+               factor: int = 2) -> List:
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array: List = [12, 72, 91]
+
+zoom_2x: List = zoom_array(tuple(array))
+
+zoom_3x: List = zoom_array(tuple(array), 3)
