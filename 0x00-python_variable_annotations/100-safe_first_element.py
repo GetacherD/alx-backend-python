4fd898ecf14e17c8cdde863a5930ef828656205c
GetacherD <getacher.d@gmail.com>
2023-01-05 22:15:57 +0300

    Any type

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,9 +4,10 @@ annotation demos
 """
 from typing import Union
 from typing import Sequence
+from typing import Any
 
 
-def safe_first_element(lst: Sequence[any]) -> Union[any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """ annotation demo """
     if lst:
         return lst[0]
