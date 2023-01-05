396a4a2a45a0215aaab63f2bbc3d19b3b098ffdf
GetacherD <getacher.d@gmail.com>
2023-01-05 21:18:11 +0300

    first

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+annotation demos
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ takes float list and return their sum as float """
+    return sum(input_list)
