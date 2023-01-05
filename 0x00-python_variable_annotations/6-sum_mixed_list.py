9a58d1e90a43bafd2c6a6cc4a57368c12fbc0f25
GetacherD <getacher.d@gmail.com>
2023-01-05 21:32:28 +0300

    order matter?

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,9 +3,10 @@
 annotation demos
 """
 from typing import Union
+from typing import List
 
 
-def sum_mixed_list(mxd_list: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """ takes float list and return their sum as float """
     sm: float = 0.0
     for i in mxd_list:
