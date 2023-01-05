b6cf03469710886cff49cf973ab77d249a9b53da
GetacherD <getacher.d@gmail.com>
2023-01-05 22:47:37 +0300

    add missing function doc

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,6 +7,7 @@ from typing import Tuple, List
 
 def zoom_array(lst: Tuple,
                factor: int = 2) -> List:
+    """ use of mypy """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
