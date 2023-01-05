746258109f463c40f0e77e3f307c9eb453e3be19
GetacherD <getacher.d@gmail.com>
2023-01-05 21:29:17 +0300

    list annotation in older python

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,11 +2,10 @@
 """
 annotation demos
 """
-from typing import List
 from typing import Union
 
 
-def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_list: list[Union[float, int]]) -> float:
     """ takes float list and return their sum as float """
     sm: float = 0.0
     for i in mxd_list:
