520e147edd1371df50f613f76ac0eaf72f4b4d1d
GetacherD <getacher.d@gmail.com>
2023-01-05 21:49:10 +0300

    return function

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+annotation demos
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[float, float]:
+    """ """
+    return lambda x: x * multiplier
