09ffdb5a92d0084d6378775e992f2d48dbb2f96e
GetacherD <getacher.d@gmail.com>
2023-01-10 15:02:42 +0300

    add default

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """ spawn function """
     res = await asyncio.gather(*[wait_random(max_delay) for i in range(n)])
     return res
