c1db70d68d3aa9abf425d10b6dbeb4cf3a6a8a02
GetacherD <getacher.d@gmail.com>
2023-01-10 14:54:41 +0300

    first

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,6 +8,12 @@ from random import randrange
 
 async def wait_random(max_delay: int = 10) -> float:
     """ Get random number wait and return it """
+    ret = -1
+    if max_delay <= 0:
+        ret = 0
+        max_delay = 1
     rn = randrange(0, max_delay)
     await asyncio.sleep(rn)
+    if ret == 0:
+        return ret
     return rn
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Python async demo
+"""
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ spawn function """
+    res = await asyncio.gather(*[wait_random(max_delay) for i in range(n)])
+    return res
