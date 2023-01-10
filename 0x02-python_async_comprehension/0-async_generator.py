25bcf5cfbdc80b258b83359e80c0f39ba13b6d5e
GetacherD <getacher.d@gmail.com>
2023-01-10 21:38:54 +0300

    Gen

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ async demo
 """
 import asyncio
 from random import uniform
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ async generator """
     for _ in range(10):
         yield uniform(0, 10)
