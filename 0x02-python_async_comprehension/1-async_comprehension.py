ac8bd0c7a50fd155cb41c2844c0fbfd28ef4de08
GetacherD <getacher.d@gmail.com>
2023-01-10 21:05:49 +0300

    first

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+async demo
+"""
+import asyncio
+from random import random
+from typing import AsyncGenerator, List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """ async generator """
+    res = [i async for i in async_generator()]
+    return res
