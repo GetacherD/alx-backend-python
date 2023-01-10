becd149c70b61f76fc7e93c7178839fc0affe0db
GetacherD <getacher.d@gmail.com>
2023-01-10 20:08:11 +0300

    first

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Python async demo
+"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines.py').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """ spawn function """
+    t1 = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    t2 = time.time()
+    return (t2 - t1) / n
