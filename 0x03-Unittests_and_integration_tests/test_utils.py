d5dca74d4599bda1cd299cbd7af6d384feccfe5d
GetacherD <getacher.d@gmail.com>
2023-01-27 12:55:14 +0300

    first

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Test nested_map
+"""
+from unittest.mock import patch
+from unittest import TestCase
+from parameterized import parameterized
+from utils import access_nested_map, get_json
+
+
+class TestAccessNestedMap(TestCase):
+    """ Class to test for nested_map"""
+    nested_map1 = {"a": 1}
+    nested_map2 = {"a": {"b": 2}}
+    nested_map3 = {"a": {"b": 2}}
+    path3 = ("a", "b")
+    path2 = ("a",)
+    path1 = ("a",)
+    nested_map4: dict = {}
+    nested_map5 = {"a": 1}
+    path4 = ("a",)
+    path5 = ("a", "b")
+
+    @parameterized.expand([
+        ([nested_map1, path1], 1),
+        ([nested_map2, path2], {"b": 2}),
+        ([nested_map3, path3], 2)
+    ])
+    def test_access_nested_map(self, nmap, out):
+        """ test for access_nested_map """
+        self.assertEqual(access_nested_map(*nmap), out)
+
+    @parameterized.expand([
+        (nested_map4, path4, KeyError('a')),
+        (nested_map5, path5, KeyError('b'))
+    ])
+    def test_access_nested_map_exception(self, nmap, path, out):
+        with self.assertRaises(KeyError) as exc:
+            access_nested_map(nmap, path)
+        self.assertIsInstance(exc.exception, KeyError)
+        self.assertEqual(str(exc.exception), str(out))
+
+
+class TestGetJson(TestCase):
+    """ Test for get json """
+
+    test_url_1 = "http://example.com"
+    test_payload_1 = {"payload": True}
+    test_url_2 = "http://holberton.io"
+    test_payload_2 = {"payload": False}
+
+    @parameterized.expand([
+        (test_url_1, test_payload_1),
+        (test_url_2, test_payload_2)
+    ])
+    def test_get_json(self, url, out):
+        """ test get json method"""
+        self.patcher = patch("utils.requests.get")
+        self.patcher.return_value = "None"
+        self.patcher.start()
+        with patch("utils.get_json") as mock_get:
+            mock_get.return_value = out
+            self.assertEqual(get_json(url), out)
+        self.patcher.stop()
