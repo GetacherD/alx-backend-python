12e11c2ac1d7316b2d3cf2e3e38b00016cbfdc3d
GetacherD <getacher.d@gmail.com>
2023-01-29 11:28:02 +0300

    use only mock asserts

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,22 +11,6 @@ from parameterized import parameterized
 class TestGithubOrgClient(unittest.TestCase):
     """ Class for testing client module """
 
-    '''
-    @parameterized.expand([
-        ("google", {"google": "google"}),
-        ("abc", {"abc": "abc"})
-    ])
-    @patch("client.get_json")
-    def test_org(self, org, output, mk_get_json):
-        """ Test Org property"""
-
-        gcl = client.GithubOrgClient(org)
-        url = gcl.ORG_URL.format(org=org)
-        mk_get_json.return_value = output
-        self.assertEqual(output, gcl.org)
-        mk_get_json.assert_called_once_with(url)
-
-    '''
     @parameterized.expand([
         ("google"),
         ("abc")
