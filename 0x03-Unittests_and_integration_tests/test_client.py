576c5a51689fb3813a1b4f196409bd8a73f8babe
GetacherD <getacher.d@gmail.com>
2023-01-29 11:21:41 +0300

    first

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,15 +11,16 @@ from parameterized import parameterized
 class TestGithubOrgClient(unittest.TestCase):
     """ Class for testing client module """
 
-    @parameterized.expand(
-        [("google", {"google": "google.github"}), (
-            "abc", {"abc": "abc.github"})])
-    def test_org(self, org, output):
+    @parameterized.expand([
+        ("google", {"google": "google"}),
+        ("abc", {"abc": "abc"})
+    ])
+    @patch("client.get_json")
+    def test_org(self, org, output, mk_get_json):
         """ Test Org property"""
-        with patch("client.get_json") as mk_get_json:
-            mk_get_json.return_value = output
-            gcl = client.GithubOrgClient(org)
-            url = gcl.ORG_URL.format(org=org)
-            self.assertEqual(gcl.org, output)
-            self.assertEqual(gcl.org, output)
-            mk_get_json.assert_called_once_with(url)
+
+        gcl = client.GithubOrgClient(org)
+        url = gcl.ORG_URL.format(org=org)
+        mk_get_json.return_value = output
+        self.assertEqual(output, gcl.org)
+        mk_get_json.assert_called_once_with(url)
