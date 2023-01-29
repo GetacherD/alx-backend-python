6f999f9a3b9838e0a538e6e2c29abb0df5d5e240
GetacherD <getacher.d@gmail.com>
2023-01-29 12:56:02 +0300

    context patch

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -31,3 +31,46 @@ class TestGithubOrgClient(unittest.TestCase):
             my_org.return_value = {"repos_url": "www.google.com"}
             gc = client.GithubOrgClient("google")
             self.assertEqual(gc._public_repos_url, "www.google.com")
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_json):
+        """ Test public_repos """
+        mock_json.return_value = [{'license': {'key': 'apache-2.0',
+                                               'name': 'Apache License 2.0',
+                                               'node_id': 'MDc6TGljZW5zZTI=',
+                                               'spdx_id': 'Apache-2.0'},
+                                   'name': 'truth'
+                                   },
+                                  {
+            'license': None,
+            'name': 'ruby-openid-apps-discovery'
+        },
+            {
+                'license': {'key': 'apache-2.0',
+                            'name': 'Apache License 2.0',
+                            'node_id': 'MDc6TGljZW5zZTI=',
+                            'spdx_id': 'Apache-2.0',
+                            },
+                'name': 'autoparse'
+        },
+            {
+                                      'license': {'key': 'other',
+                                                  'name': 'Other',
+                                                  'node_id': 'MDc6TG=',
+                                                  'spdx_id': 'NOASSERTION',
+                                                  'url': None},
+
+                                      'name': 'anvil-build'}
+        ]
+        with patch("client.GithubOrgClient._public_repos_url"
+                   ) as mock_repo_url:
+            mock_repo_url.return_value = "https://api.google.com/repos"
+            gc = client.GithubOrgClient("google")
+            gc._public_repos_url()
+            self.assertEqual(gc.public_repos(), [
+                             'truth',
+                             'ruby-openid-apps-discovery',
+                             'autoparse', 'anvil-build'])
+            self.assertEqual(gc.public_repos("other"), ['anvil-build'])
+            mock_json.assert_called_once()
+            mock_repo_url.assert_called_once()
